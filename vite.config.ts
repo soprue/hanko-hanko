afeda9ac5aa529a0b26dc8411437cbc740fb20b5
soprue <soprue@naver.com>
2025-07-17 18:58:11 +0900

    chore: alias path 설정

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,14 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '/': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
 });
