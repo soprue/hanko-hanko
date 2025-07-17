9e37b300bfc24c9eff99a72df669f507e79e9920
soprue <soprue@naver.com>
2025-07-17 18:25:21 +0900

    chore: prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,44 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       parserOptions: {
         ecmaFeatures: { jsx: true },
-        project: true,
+        project: [
+          './tsconfig.json',
+          './tsconfig.app.json',
+          './tsconfig.node.json',
+        ],
       },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
-      "react/react-in-jsx-scope": "off", // React 17+ 부터 JSX에서 import 안 해도 되므로 끔
-      "react/prop-types": "off", // TS 쓰면 prop-types 필요 없으므로 끔
-      "@typescript-eslint/no-unused-vars": ["warn"], // TS용 미사용 변수 경고
+      'react/react-in-jsx-scope': 'off', // React 17+ 부터 JSX에서 import 안 해도 되므로 끔
+      'react/prop-types': 'off', // TS 쓰면 prop-types 필요 없으므로 끔
+      '@typescript-eslint/no-unused-vars': ['warn'], // TS용 미사용 변수 경고
     },
     settings: {
       react: {
-        version: "detect", // 설치된 React 버전 자동 감지
+        version: 'detect', // 설치된 React 버전 자동 감지
       },
     },
-    ignorePatterns: [".eslintrc.cjs"],
+    ignores: ['eslint.config.js'],
   },
   eslintConfigPrettier,
 ]);
