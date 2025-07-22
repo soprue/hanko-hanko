b036be8fb77d9086ae1c6044c89fde0f26b20451
soprue <soprue@naver.com>
2025-07-22 22:04:02 +0900

    feat: 헤더 UI 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,12 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className='sticky top-0 h-20 w-full shadow-[0_1px_4px_0_rgba(0,0,0,0.1)]'>
+      <div className='container mx-auto flex justify-between'>
+        <img src='/assets/images/logo.png' alt='logo' className='w-20' />
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import typography from '@tailwindcss/typography';
+
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [typography],
+};
