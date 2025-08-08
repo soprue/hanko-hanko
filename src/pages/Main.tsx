49fe3228bcbf3c9b38974b6f2ebb70e3bae92de1
soprue <soprue@naver.com>
2025-08-08 21:42:14 +0900

    feat: CheckBox 컴포넌트 생성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,22 +1,37 @@
+import { useState } from 'react';
+
+import CheckBox from '@/components/ui/CheckBox';
 import Icon from '@/components/ui/Icon';
 import Button from '@components/ui/Button';
 import Card from '@components/ui/Card';
 
 function Main() {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div className='max-wide:px-10 flex min-h-[calc(100vh-80px)] min-w-[1440px] items-center bg-[url(/assets/images/background.webp)] bg-cover py-15'>
       <div className='mx-auto flex w-[1440px] justify-between gap-12'>
         <Card>
-          <Button variant='ghost'>zz</Button>
-          <Button variant='ghost' size='sm'>
-            zz
-          </Button>
-          <Button variant='ghost' size='lg'>
-            zz
-          </Button>
-          <Button>zz</Button>
+          <div className='py-5'>
+            <Button
+              onClick={() => setIsButtonClicked(!isButtonClicked)}
+              variant={isButtonClicked ? 'ghost' : 'default'}
+              size='sm'
+            >
+              zz
+            </Button>
+            <Button variant='ghost' disabled>
+              zz
+            </Button>
+            <Button variant='ghost' size='lg'>
+              zz
+            </Button>
+            <Button>zz</Button>
+            <Button disabled>zz</Button>
+          </div>
 
-          <div className='flex items-center gap-2 py-10'>
+          <div className='py-5'>
             <Icon
               name='checkbox'
               // color='red'
@@ -24,7 +39,16 @@ function Main() {
               className='text-text-heading'
               onClick={() => console.log('click')}
             />
-            그룹 사용
+          </div>
+
+          <div className='flex flex-col justify-center gap-2 py-5'>
+            <CheckBox onChange={() => {}} label='비활성화' disabled size='lg' />
+            <CheckBox
+              checked={isChecked}
+              onChange={setIsChecked}
+              label='그룹 사용'
+            />
+            <CheckBox onChange={() => {}} label='비활성화' disabled size='sm' />
           </div>
         </Card>
         <Card>Card</Card>
