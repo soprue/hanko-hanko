48abeb8591eabd2ddaf88cdfe86a66046c5082cf
soprue <soprue@naver.com>
2025-08-09 16:41:07 +0900

    feat: Input 컴포넌트 생성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,10 +4,12 @@ import CheckBox from '@/components/ui/CheckBox';
 import Icon from '@/components/ui/Icon';
 import Button from '@components/ui/Button';
 import Card from '@components/ui/Card';
+import Input from '@/components/ui/Input';
 
 function Main() {
   const [isButtonClicked, setIsButtonClicked] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
+  const [inputValue, setInputValue] = useState('');
 
   return (
     <div className='max-wide:px-10 flex min-h-[calc(100vh-80px)] min-w-[1440px] items-center bg-[url(/assets/images/background.webp)] bg-cover py-15'>
@@ -49,6 +51,18 @@ function Main() {
               label='그룹 사용'
             />
             <CheckBox onChange={() => {}} label='비활성화' disabled size='sm' />
+          </div>
+
+          <div className='py-5'>
+            <Input value={inputValue} onChange={setInputValue} />
+            <Input value={inputValue} onChange={setInputValue} disabled />
+            <Input value={inputValue} onChange={setInputValue} readOnly />
+            <Input
+              value={inputValue}
+              onChange={setInputValue}
+              fullWidth
+              error='⚠ 이전 단보다 코 수가 증가하거나 감소했지만, 늘림뜨기나 줄임뜨기가 사용되지 않았어요.'
+            />
           </div>
         </Card>
         <Card>Card</Card>
