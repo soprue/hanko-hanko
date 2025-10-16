1b1c5ef185b871a3eb756297aa1f649c45bf68d7
soprue <soprue@naver.com>
2025-10-16 15:52:53 +0900

    chore: setnry 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 
+import * as Sentry from '@sentry/react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import '@/index.css';
 import App from '@/App.tsx';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: true,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
