193db68802be93a851e3e29f6ee673f2007c6d6e
soprue <soprue@naver.com>
2025-10-16 16:14:22 +0900

    feat: sentry 상태 무결성 감시 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from '@components/layout/Layout';
+import StoreWatcher from '@components/StoreWatcher';
 import GlobalModal from '@components/ui/GlobalModal';
 import Main from '@pages/Main';
 
 function App() {
   return (
     <>
+      <StoreWatcher />
       <GlobalModal />
 
       <Routes>
